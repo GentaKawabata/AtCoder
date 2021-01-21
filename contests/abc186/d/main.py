598c1e1017ce6867eb53a383965f077849826fbb
kawabata(laptop) <g.kawabata@jrsi.co.jp>
2021-01-22 08:19:13 +0900

    abc186

--- contests/abc186/d/main.py
+++ contests/abc186/d/main.py
@@ -1,20 +1,40 @@
 # -*- coding: utf-8 -*-
-from copy import copy
 
 
-def get_ruiseki_wa(in_list):
-    if len(in_list) == 0:
+from typing import List
+
+
+def get_cumulative_sum_recursive(numbers: List[int]):
+    """再帰で累積和を求める。
+    RecursionError が出てしまうケース
+    """
+    if len(numbers) == 0:
         return [0]
     else:
-        tail = in_list.pop(-1)
-        ruiseki_list = get_ruiseki_wa(in_list)
+        # tail = numbers.pop(-1)
+        tail = numbers[-1]
+        ruiseki_list = get_cumulative_sum_recursive(numbers[0:len(numbers) - 1])
 
         tail_of_pre = ruiseki_list[-1]
         return ruiseki_list + [tail_of_pre + tail]
 
 
-def get_answer_ij(l_a):
+def get_cumulative_sum_sequential(numbers: List[int]):
+    """forで累積和を求める。
+    This method works.
+    """
+    result: List[int] = [0]
 
+    for number in numbers:
+        result.append(result[-1] + number)
+
+    return result
+
+
+def _get_answer_primitive(l_a: List[int]):
+    """forと再帰で答えを求める。
+    O(N^2)
+    """
     if len(l_a) == 2:
         return abs(l_a[0] - l_a[1])
     else:
@@ -23,13 +43,31 @@ def get_answer_ij(l_a):
             total += abs(l_a[k] - l_a[-1])
 
         _ = l_a.pop(-1)
-        return get_answer_ij(l_a) + total
+        return _get_answer_primitive(l_a) + total
 
 
-def get_answer(n, l_a):
+def _get_answer_cumulative(l_a: List[int]):
+    """累積和で答えを求める。
+    O(N*logN)
+    """
+    n = len(l_a)
+    l_c = get_cumulative_sum_sequential(l_a)
+
+    # (N-1) * C_N+1
+    ans1 = (n - 1) * l_c[-1]
+
+    # sigma{i=1>N-1}{C_i + (N-i) * A_i}
+    ans2 = 0
+    for i in range(n - 1):
+        ans2 += l_c[i] + (n - i) * l_a[i]
+
+    return ans1 - ans2
+
+
+def get_answer(n: int, l_a: List[int]):
     l_a.sort()
 
-    return get_answer_ij(l_a)
+    return _get_answer_cumulative(l_a)
 
 
 if __name__ == "__main__":
--- contests/abc186/d/test.py
+++ contests/abc186/d/test.py
@@ -2,7 +2,8 @@
 
 from unittest import TestCase
 
-from contests.abc186.d.main import *
+import contests.abc186.d.main as main
+from tests.utils import MyTimer
 
 
 class Test(TestCase):
@@ -17,26 +18,64 @@ class Test(TestCase):
             L_A
         )
 
-    def test_get_ruiseki_wa_1(self):
+    def test_get_cumulative_sum_1(self):
         L_A = [1, 2, 5]
 
         exp = [0, 1, 3, 8]
-        act = get_ruiseki_wa(L_A)
+        act = main.get_cumulative_sum_sequential(L_A)
 
         self.assertEqual(exp, act)
 
-    def test_get_ruiseki_wa_2(self):
+    def test_get_cumulative_sum_2(self):
         L_A = [-2, 0, 3, 9, 15, 26]
 
         exp = [0, -2, -2, 1, 10, 25, 51]
-        act = get_ruiseki_wa(L_A)
+        act = main.get_cumulative_sum_sequential(L_A)
 
         self.assertEqual(exp, act)
 
-    def test_get_ruiseki_wa_3(self):
+    def test_get_cumulative_sum_3(self):
         L_A = [1, 6, 3, 8, 4, 2, 9, 5, 7]
 
         exp = [0, 1, 7, 10, 18, 22, 24, 33, 38, 45]
-        act = get_ruiseki_wa(L_A)
+        act = main.get_cumulative_sum_sequential(L_A)
 
         self.assertEqual(exp, act)
+
+    def test_get_cumulative_sum_unsorted(self):
+        L_A_orig = [5, 3, 8, 2, 9, 1]
+        L_A = L_A_orig.copy()
+
+        exp = [0, 5, 8, 16, 18, 27, 28]
+
+
+        timer: MyTimer = MyTimer()
+        timer.start()
+        act = main.get_cumulative_sum_sequential(L_A)
+        timer.stop()
+        print(timer.message)
+
+        with self.subTest(name="CumulativeSum"):
+            self.assertEqual(exp, act)
+        with self.subTest(name="ComparisonListBeforeAfter"):
+            self.assertEqual(L_A_orig, L_A)
+
+    def test_get_answer_measure_time(self):
+        import random
+
+        N = 2 * pow(10, 5)
+
+        L_A = []
+        for i in range(N):
+            L_A.append(random.randint(0, pow(10, 8)))
+
+        timer = MyTimer()
+        timer.start()
+
+        # ans = main._get_answer_primitive(L_A)
+        ans = main._get_answer_cumulative(L_A)
+
+        timer.stop()
+        print(timer.message)
+
+        self.assertTrue(True)
