d6568051d633fc7ad51cb594b9d3035de9bbb645
kawabata(laptop) <g.kawabata@jrsi.co.jp>
2021-01-23 18:40:54 +0900

    ダウンロードとテスト環境の作成

--- atcoder-cli_download.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set contest_id=abc186
-
-
-acc new %contest_id%
-
-pause
-
---
+++ make_test_env.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+
+TESTS_DIR = "./tests"
+TEMPLATE_FILE = "./test_template.txt"
+
+if __name__ == "__main__":
+
+    current_dir = os.path.abspath("./")
+
+    # tests ディレクトリの作成
+    tests_dir_path: str = os.path.abspath(TESTS_DIR)
+    if not os.path.exists(tests_dir_path):
+        os.makedirs(tests_dir_path)
+
+    # オプションからコンテストID を取得
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--contest", help="Contest ID", required=True, type=str)
+    args = parser.parse_args()
+    contest_id = args.contest
+
+    # コンテストIDディレクトリの中にある各問題の名前を取得
+    files_dirs = os.listdir(os.path.join(current_dir, contest_id))
+    list_question_dir = [dir for dir in files_dirs if os.path.isdir(os.path.join(current_dir, contest_id, dir))]
+
+    # テストコードのテンプレートを取得
+    for question_name in list_question_dir:
+        with open(os.path.abspath(TEMPLATE_FILE), "r") as fr:
+            test_code = fr.read()
+            
+            # コンテストIDと問題名を置換
+            new_test_code = test_code.replace("{contest_id}", contest_id)
+            new_test_code = new_test_code.replace("{question}", question_name)
+
+            # テストコードファイル作成
+            test_file_path = os.path.join(tests_dir_path, f"test_{contest_id}_{question_name}.py")
+            with open(test_file_path, "w") as fw:
+                fw.write(new_test_code)
