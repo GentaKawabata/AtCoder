679336e62cce1c72b1d4855ce9ac07e7c9dcedef
kawabata(laptop) <g.kawabata@jrsi.co.jp>
2021-02-19 18:00:39 +0900

    ab179

---
+++ tests/test_abc179_e.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+import random as rd
+
+# import tests.utils as utils
+# import utils
+from abc179.e.main import *
+
+class Test_abc179_e(TestCase):
+
+    def test_get_answer_1(self):
+        self.fail()
